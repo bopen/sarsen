bb0b75f1aa586d0ef5335a57fcc8e87e0cb64b0e
Alessandro Amici <a.amici@bopen.eu>
2021-12-12 19:44:29 +0100

    Accept the fact that we need conda-forge

--- tests/test_10_scene.py
+++ tests/test_10_scene.py
@@ -1,5 +1,6 @@
 import os.path
 
+import numpy as np
 import pytest
 import rioxarray
 import xarray as xr
@@ -23,3 +24,24 @@ def test_make_dem_3d(dem_raster: xr.DataArray) -> None:
     assert res.dims == ("axis", "y", "x")
     assert res.name == "dem_3d"
     assert res.sel(x=12.5, y=42, method="nearest")[2] == 17.0
+
+
+def test_transform_dem_3d(dem_raster: xr.DataArray) -> None:
+    dem_3d = scene.make_dem_3d(dem_raster)
+
+    # from height over the geoid to height over the ellipsoid
+    res = scene.transform_dem_3d(dem_3d, "EPSG:4979")
+
+    assert res.dims == ("axis", "y", "x")
+    # this assert fails if proj-data is not properly installed on the system
+    assert abs(res.sel(x=12.5, y=42, method="nearest")[2] - 65.613) < 0.001
+
+    expected = [4634523.742, 1027449.178, 4245647.74]
+
+    # from geographic to geocentric (ECEF)
+    res = scene.transform_dem_3d(dem_3d, "EPSG:4978")
+
+    assert res.dims == ("axis", "y", "x")
+    assert np.allclose(
+        res.sel(x=12.5, y=42, method="nearest"), expected, rtol=0, atol=0.001
+    )
