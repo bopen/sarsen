0232469c51560544dec3d1ef1e22ce74fa54437f
Alessandro Amici <a.amici@bopen.eu>
2022-01-15 08:40:26 +0100

    First app: backward geocoding of a IW burst

---
+++ notebooks/download_data.py
@@ -0,0 +1,22 @@
+import os
+
+import elevation
+import sentinelsat
+
+data = [
+    ("*iw3*vv*", "S1B_IW_SLC__1SDV_20211223T051121_20211223T051148_030148_039993_BA4B"),
+]
+
+
+user = os.environ["DHUS_USER"]
+password = os.environ["DHUS_PASSWORD"]
+
+api = sentinelsat.SentinelAPI(user, password)
+
+for filter, identifier in data:
+    nodefilter = sentinelsat.make_path_filter(filter)
+    s1b_rome = api.query(identifier=identifier)
+    api.download_all(s1b_rome, "./data", nodefilter=nodefilter)
+
+
+# elevation.clip(bounds=(12.35, 41.91, 12.65, 42.12), output="./data/Rome-30m-DEM.tif")
---
+++ sarsen/apps.py
@@ -0,0 +1,56 @@
+import xarray as xr
+
+from . import geocoding, orbit, scene
+
+
+def backward_geocode_sentinel1_slc_iw_burst(
+    product_urlpath: str,
+    measurement_group: str,
+    dem_urlpath: str,
+    output_urlpath: str = "GRD.tif",
+) -> None:
+    swath_polarization_id, _, burst_index = measurement_group.rpartition("/")
+    orbit_group = f"{swath_polarization_id}/orbit"
+
+    print("open data")
+
+    burst_sar = xr.open_dataset(product_urlpath, engine="sentinel-1", group=measurement_group)  # type: ignore
+    orbit_ecef = xr.open_dataset(product_urlpath, engine="sentinel-1", group=orbit_group)  # type: ignore
+    dem_raster = scene.open_dem_raster(dem_urlpath)
+
+    print("process DEM")
+
+    dem_3d = scene.make_dem_3d(dem_raster)
+    dem_ecef = scene.transform_dem_3d(dem_3d)
+
+    print("interpolate orbit")
+
+    orbit_interpolator = orbit.OrbitPolyfitIterpolator.from_position(
+        orbit_ecef.position
+    )
+    position_sar = orbit_interpolator.position(burst_sar.azimuth_time)
+    velocity_sar = orbit_interpolator.velocity(burst_sar.azimuth_time)
+
+    print("geocode")
+
+    dem_coords = geocoding.backward_geocode(
+        dem_ecef, position_sar.azimuth_time, position_sar, velocity_sar
+    )
+
+    print("interpolate")
+
+    geocoded = abs(burst_sar).sel(
+        azimuth_time=dem_coords.azimuth_time,
+        slant_range_time=dem_coords.slant_range_time,
+        method="nearest",
+    )
+    geocoded.rio.set_crs(dem_raster.rio.crs)
+
+    geocoded.rio.to_raster(
+        output_urlpath,
+        tiled=True,
+        blockxsize=512,
+        blockysize=512,
+        compress="DEFLATE",
+        num_threads="ALL_CPUS",
+    )
