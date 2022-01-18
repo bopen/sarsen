401f585669b13ec51fbd6806279873b3d1e9e7ce
Alessandro Amici <a.amici@bopen.eu>
2022-01-18 12:27:51 +0100

    No need to download multiple close images

--- notebooks/download_data.sh
+++ notebooks/download_data.sh
@@ -7,11 +7,7 @@
     data/Rome-30m-DEM.tif data/Rome-10m-DEM.tif
 
 sentinelsat --path data -d --include-pattern "*iw3*vv*" \
-  --name S1A_IW_SLC__1SDV_20211229T051151_20211229T051218_041219_04E60D_F691
-sentinelsat --path data -d --include-pattern "*iw3*vv*" \
   --name S1B_IW_SLC__1SDV_20211223T051121_20211223T051148_030148_039993_BA4B
-sentinelsat --path data -d --include-pattern "*iw3*vv*" \
-  --name S1A_IW_SLC__1SDV_20211217T051152_20211217T051219_041044_04E03C_75D1
 
 # Chicago SM EPSG:32616 5x5
 
@@ -21,8 +17,4 @@ sentinelsat --path data -d --include-pattern "*iw3*vv*" \
     data/Chicago-30m-DEM.tif data/Chicago-5m-DEM.tif
 
 sentinelsat --path data -d --include-pattern "*vv*" \
-  --name S1A_S6_SLC__1SDV_20211222T115519_20211222T115543_041121_04E2C7_EAA4
-sentinelsat --path data -d --include-pattern "*vv*" \
   --name S1B_S6_SLC__1SDV_20211216T115438_20211216T115501_030050_03968A_4DCB
-sentinelsat --path data -d --include-pattern "*vv*" \
-  --name S1A_S6_SLC__1SDV_20211210T115520_20211210T115543_040946_04DCEB_0E6D
