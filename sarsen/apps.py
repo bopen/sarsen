58c06b5237f3e7e6e8bb145ea32d578d907fb954
Alessandro Amici <a.amici@bopen.eu>
2022-11-01 22:38:46 +0100

    Sync part of the docstring

--- sarsen/apps.py
+++ sarsen/apps.py
@@ -68,9 +68,6 @@ def terrain_correction(
 
     :param product: SarProduct instance representing the input data
     :param dem_urlpath: dem path or url
-    :param orbit_group: overrides the orbit group name
-    :param calibration_group: overrides the calibration group name
-    :param coordinate_conversion_group: overrides the coordinate_conversion group name
     :param output_urlpath: output path or url
     :param correct_radiometry: default `None`. If `correct_radiometry=None`the radiometric terrain
     correction is not applied. `correct_radiometry=gamma_bilinear` applies the gamma flattening classic
@@ -79,12 +76,12 @@ def terrain_correction(
     'gamma_nearest' significantly reduces the processing time
     :param interp_method: interpolation method for product resampling.
     The interpolation methods are the methods supported by ``xarray.DataArray.interp``
-    :param multilook: multilook factor. If `None` the multilook is not applied
     :param grouping_area_factor: is a tuple of floats greater than 1. The default is `(1, 1)`.
     The `grouping_area_factor`  can be increased (i) to speed up the processing or
     (ii) when the input DEM resolution is low.
     The Gamma Flattening usually works properly if the pixel size of the input DEM is much smaller
-    than the pixel size of the input Sentinel-1 product. Otherwise, the output may have radiometric distortions.
+    than the pixel size of the input Sentinel-1 product.
+    Otherwise, the output may have radiometric distortions.
     This problem can be avoided by increasing the `grouping_area_factor`.
     Be aware that `grouping_area_factor` too high may degrade the final result
     :param open_dem_raster_kwargs: additional keyword arguments passed on to ``xarray.open_dataset``
