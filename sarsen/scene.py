c8b609590d1c7544f43e7d8e2716f2a115aef0cf
Alessandro Amici <a.amici@bopen.eu>
2024-12-29 16:32:53 +0100

    Fix typing

--- sarsen/scene.py
+++ sarsen/scene.py
@@ -82,7 +82,9 @@ def transform_dem_3d(
     return dem_3d_crs
 
 
-def upsample(data: xr.DataArray, dtype: str = None, **factors: int) -> xr.DataArray:
+def upsample(
+    data: xr.DataArray, dtype: str | None = None, **factors: int
+) -> xr.DataArray:
     coords = {}
     for dim, factor in factors.items():
         coord = data.coords[dim]
