b8be17a0d70f4d81ca5a2a11a694b09b03574958
Alessandro Amici <a.amici@proton.me>
2026-09-10 20:06:24 +0200

    Add SLC SM tests

--- tests/test_50_apps.py
+++ tests/test_50_apps.py
@@ -16,31 +16,40 @@ DATA_PATHS = [
     / "S1A_IW_SLC__1SDV_20220104T170557_20220104T170624_041314_04E951_F1F1.SAFE",
     DATA_FOLDER
     / "S1A_IW_SLC__1SDV_20220104T170557_20220104T170624_041314_04E951_F1F1.SAFE",
+    DATA_FOLDER
+    / "S1A_S3_SLC__1SDV_20210401T152855_20210401T152914_037258_04638E_6001.SAFE",
 ]
 
-GROUPS = ["IW/VV", "IW1/VV", "IW1/VV/2"]
+GROUPS = ["IW/VV", "IW1/VV", "IW1/VV/2", "S3/VH"]
 
-DATA_PATH_GROUPS = list(zip(DATA_PATHS, GROUPS))
+DEM_RASTERS = [
+    DATA_FOLDER / "Rome-30m-DEM.tif",
+    DATA_FOLDER / "Rome-30m-DEM.tif",
+    DATA_FOLDER / "Rome-30m-DEM.tif",
+    DATA_FOLDER / "Comoros-30m-DEM.tif",
+]
 
-DEM_RASTER = DATA_FOLDER / "Rome-30m-DEM.tif"
+DATA_PATH_GROUP_DEM = list(zip(DATA_PATHS, GROUPS, DEM_RASTERS))
 
 
-@pytest.mark.parametrize("data_path,group", DATA_PATH_GROUPS)
+@pytest.mark.parametrize("data_path,group,dem_raster", DATA_PATH_GROUP_DEM)
 @pytest.mark.skipif(os.getenv("GITHUB_ACTIONS") == "true", reason="too much memory")
 def test_terrain_correction_gtc(
     tmpdir: py.path.local,
     data_path: pathlib.Path,
     group: str,
+    dem_raster: pathlib.Path,
 ) -> None:
     out = str(tmpdir.join("GTC.tif"))
     product = sentinel1.Sentinel1SarProduct(
         str(data_path),
         group,
+        measurement_chunks={},
     )
 
     res = apps.terrain_correction(
         product,
-        str(DEM_RASTER),
+        str(dem_raster),
         output_urlpath=out,
     )
 
@@ -48,20 +57,21 @@ def test_terrain_correction_gtc(
     assert "beta" in res.attrs["long_name"]
 
 
-@pytest.mark.parametrize("data_path,group", DATA_PATH_GROUPS)
+@pytest.mark.parametrize("data_path,group,dem_raster", DATA_PATH_GROUP_DEM)
 @pytest.mark.skipif(os.getenv("GITHUB_ACTIONS") == "true", reason="too much memory")
 def test_terrain_correction_fast_rtc(
-    tmpdir: py.path.local, data_path: pathlib.Path, group: str
+    tmpdir: py.path.local, data_path: pathlib.Path, group: str, dem_raster: pathlib.Path
 ) -> None:
     out = str(tmpdir.join("RTC.tif"))
     product = sentinel1.Sentinel1SarProduct(
         str(data_path),
         group,
+        measurement_chunks={},
     )
 
     res = apps.terrain_correction(
         product,
-        str(DEM_RASTER),
+        str(dem_raster),
         correct_radiometry="gamma_nearest",
         output_urlpath=out,
         seed_step=(32, 32),
@@ -71,20 +81,21 @@ def test_terrain_correction_fast_rtc(
     assert "gamma" in res.attrs["long_name"]
 
 
-@pytest.mark.parametrize("data_path,group", DATA_PATH_GROUPS)
+@pytest.mark.parametrize("data_path,group,dem_raster", DATA_PATH_GROUP_DEM)
 @pytest.mark.skipif(os.getenv("GITHUB_ACTIONS") == "true", reason="too much memory")
 def test_terrain_correction_rtc(
-    tmpdir: py.path.local, data_path: pathlib.Path, group: str
+    tmpdir: py.path.local, data_path: pathlib.Path, group: str, dem_raster: pathlib.Path
 ) -> None:
     out = str(tmpdir.join("RTC.tif"))
     product = sentinel1.Sentinel1SarProduct(
         str(data_path),
         group,
+        measurement_chunks={},
     )
 
     res = apps.terrain_correction(
         product,
-        str(DEM_RASTER),
+        str(dem_raster),
         correct_radiometry="gamma_bilinear",
         output_urlpath=out,
     )
@@ -93,20 +104,21 @@ def test_terrain_correction_rtc(
     assert "gamma" in res.attrs["long_name"]
 
 
-@pytest.mark.parametrize("data_path,group", DATA_PATH_GROUPS)
+@pytest.mark.parametrize("data_path,group,dem_raster", DATA_PATH_GROUP_DEM)
 @pytest.mark.skipif(os.getenv("GITHUB_ACTIONS") == "true", reason="too much memory")
 def test_terrain_correction_gtc_dask(
-    tmpdir: py.path.local, data_path: pathlib.Path, group: str
+    tmpdir: py.path.local, data_path: pathlib.Path, group: str, dem_raster: pathlib.Path
 ) -> None:
     out = str(tmpdir.join("GTC.tif"))
     product = sentinel1.Sentinel1SarProduct(
         str(data_path),
         group,
+        measurement_chunks={},
     )
 
     res = apps.terrain_correction(
         product,
-        str(DEM_RASTER),
+        str(dem_raster),
         output_urlpath=out,
         chunks=1024,
         seed_step=(32, 32),
@@ -116,20 +128,21 @@ def test_terrain_correction_gtc_dask(
     assert "beta" in res.attrs["long_name"]
 
 
-@pytest.mark.parametrize("data_path,group", DATA_PATH_GROUPS)
+@pytest.mark.parametrize("data_path,group,dem_raster", DATA_PATH_GROUP_DEM)
 @pytest.mark.skipif(os.getenv("GITHUB_ACTIONS") == "true", reason="too much memory")
 def test_terrain_correction_fast_rtc_dask(
-    tmpdir: py.path.local, data_path: pathlib.Path, group: str
+    tmpdir: py.path.local, data_path: pathlib.Path, group: str, dem_raster: pathlib.Path
 ) -> None:
     out = str(tmpdir.join("RTC.tif"))
     product = sentinel1.Sentinel1SarProduct(
         str(data_path),
         group,
+        measurement_chunks={},
     )
 
     res = apps.terrain_correction(
         product,
-        str(DEM_RASTER),
+        str(dem_raster),
         correct_radiometry="gamma_nearest",
         output_urlpath=out,
         chunks=1024,
@@ -140,20 +153,21 @@ def test_terrain_correction_fast_rtc_dask(
     assert "gamma" in res.attrs["long_name"]
 
 
-@pytest.mark.parametrize("data_path,group", DATA_PATH_GROUPS)
+@pytest.mark.parametrize("data_path,group,dem_raster", DATA_PATH_GROUP_DEM)
 @pytest.mark.skipif(os.getenv("GITHUB_ACTIONS") == "true", reason="too much memory")
 def test_terrain_correction_rtc_dask(
-    tmpdir: py.path.local, data_path: pathlib.Path, group: str
+    tmpdir: py.path.local, data_path: pathlib.Path, group: str, dem_raster: pathlib.Path
 ) -> None:
     out = str(tmpdir.join("RTC.tif"))
     product = sentinel1.Sentinel1SarProduct(
         str(data_path),
         group,
+        measurement_chunks={},
     )
 
     res = apps.terrain_correction(
         product,
-        str(DEM_RASTER),
+        str(dem_raster),
         correct_radiometry="gamma_bilinear",
         output_urlpath=out,
         chunks=1024,
